19eba390d3aa9f14f28081828bf9cd9173c4a0d1
Albert Attard <albert.attard@thoughtworks.com>
2021-05-15 10:41:11 +0200

    Download single page

--- src/main/java/quickhacks/Main.java
+++ src/main/java/quickhacks/Main.java
@@ -1,8 +1,28 @@
 package quickhacks;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+
 public class Main {
 
-    public static void main(final String[] args) {
-        System.out.println("Find broken links");
+    public static void main(final String[] args) throws IOException, InterruptedException {
+        final HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
+
+        final String pageLink = "https://albertattard.github.io/quickhacks/";
+        final HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(pageLink))
+                .timeout(Duration.ofSeconds(5))
+                .build();
+
+        final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
