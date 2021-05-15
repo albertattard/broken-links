f8a3c4d6452f7b9c21e91f69857b2cbd4295706c
Albert Attard <albert.attard@thoughtworks.com>
2021-05-15 11:12:17 +0200

    Introduce a loop to navigate throughout the website

--- src/main/java/quickhacks/Main.java
+++ src/main/java/quickhacks/Main.java
@@ -10,6 +10,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -20,22 +22,30 @@ public class Main {
                 .connectTimeout(Duration.ofSeconds(5))
                 .build();
 
-        final String pageLink = "https://albertattard.github.io/quickhacks/some-page-that-does-not-exists";
-        final HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(pageLink))
-                .timeout(Duration.ofSeconds(5))
-                .build();
+        final String startLink = "https://albertattard.github.io/quickhacks/";
 
-        final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        if (response.statusCode() >= 400) {
-            System.out.printf("Broken link %s%n", pageLink);
-            return;
-        }
+        final List<String> pending = new ArrayList<>();
+        pending.add(startLink);
 
-        System.out.println("Found the following links:");
-        final Document document = Jsoup.parse(response.body());
-        for (Element a : document.select("a[href]")) {
-            System.out.printf("  > %s%n", a.attr("href"));
+        while (pending.isEmpty() == false) {
+
+            final String pageLink = pending.remove(0);
+            final HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(pageLink))
+                    .timeout(Duration.ofSeconds(5))
+                    .build();
+
+            final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() >= 400) {
+                System.out.printf("Broken link %s%n", pageLink);
+                return;
+            }
+
+            System.out.println("Found the following links:");
+            final Document document = Jsoup.parse(response.body());
+            for (Element a : document.select("a[href]")) {
+                System.out.printf("  > %s%n", a.attr("href"));
+            }
         }
     }
 }
