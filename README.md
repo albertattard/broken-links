5373c160a3a250df0de8396ce618a2ad9b2d8d0a
Albert Attard <albert.attard@thoughtworks.com>
2021-05-15 10:25:29 +0200

    Create skeleton project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Broken Links
+
+A simple program that searches for broken links in a website by navigating through the links until it scans the whole
+website.
---
+++ src/main/java/quickhacks/Main.java
@@ -0,0 +1,8 @@
+package quickhacks;
+
+public class Main {
+
+    public static void main(final String[] args) {
+        System.out.println("Find broken links");
+    }
+}
