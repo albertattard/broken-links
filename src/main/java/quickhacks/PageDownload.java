2cd8a3f97eb2f263b855c03f7a688b179d26e0ce
Albert Attard <albert.attard@thoughtworks.com>
2021-05-16 18:15:00 +0200

    Download single page and handle 404 (Refactor)

--- src/main/java/quickhacks/PageDownload.java
+++ src/main/java/quickhacks/PageDownload.java
@@ -9,13 +9,9 @@ import java.time.Duration;
 
 public class PageDownload {
 
-    public Result download(final String link) throws IOException, InterruptedException {
-        final HttpClient client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_1_1)
-                .followRedirects(HttpClient.Redirect.NORMAL)
-                .connectTimeout(Duration.ofSeconds(5))
-                .build();
+    private final HttpClient client = createHttpClient();
 
+    public Result download(final String link) throws IOException, InterruptedException {
         final HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(link))
                 .timeout(Duration.ofSeconds(5))
@@ -23,12 +19,20 @@ public class PageDownload {
 
         client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        return new quickhacks.PageDownload.Result(quickhacks.PageDownload.Result.Outcome.CLIENT_ERROR);
+        return new Result(Result.Outcome.CLIENT_ERROR);
     }
 
     public static record Result(Outcome outcome) {
         enum Outcome {
             CLIENT_ERROR
         }
+    }
+
+    private static HttpClient createHttpClient() {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_1_1)
+                .followRedirects(HttpClient.Redirect.NORMAL)
+                .connectTimeout(Duration.ofSeconds(5))
+                .build();
     }
 }
